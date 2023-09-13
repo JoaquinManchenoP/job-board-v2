e49a64065a2f4d20d0002fa88c8619dd110367ba
JoaquinManchenoP <jj.mancheno@gmail.com>
2023-09-13 19:29:34 +0200

    job-listing

---
+++ app/components/Header/Header.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Header() {
+  return <div className="h-16 w-full bg-white ">this is the header</div>;
+}
---
+++ app/components/JobCard/JobCard.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function JobCard() {
+  return <div className="h-48 w-full bg-blue-100 rounded-md">JobCard</div>;
+}
