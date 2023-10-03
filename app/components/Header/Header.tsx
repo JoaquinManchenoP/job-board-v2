049c064e595779f59d6cdae4e3c1609285d4c717
JoaquinManchenoP <jj.mancheno@gmail.com>
2023-10-03 14:23:43 +0200

    basic layout compleated

--- app/components/Header/Header.tsx
+++ app/components/Header/Header.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { GiHamburgerMenu } from "react-icons/gi";
 
 export default function Header() {
   return (
@@ -7,7 +8,7 @@ export default function Header() {
         Logo
       </div>
       <div className="menu h-full w-1/4 bg-gray-200 flex items-center justify-center">
-        Menu
+        <GiHamburgerMenu className="text-xl" />
       </div>
     </div>
   );
